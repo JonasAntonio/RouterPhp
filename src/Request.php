b34d2801de77d0f18bef69d810437628de579faf
JonasAntonio <jayv.jonas@gmail.com>
2022-03-01 21:24:27 -0300

    clean Request.php

--- src/Request.php
+++ src/Request.php
@@ -9,57 +9,35 @@ class Request
     */
    public static function getUrl(): string
    {
-      return $_GET['url'] ?? '';
+      return @$_GET['url'];
    }
 
    /**
     * Retorna o método da requisição
     */
-   public static function getMethod(): string
+   public function getMethod(): string
    {
       return $_SERVER['REQUEST_METHOD'];
    }
 
    /**
-    * Retorna um array com os dados recebidos na requisição
-    * @return array
+    * Retorna um array com os dados da request
     */
    public function getData(): array
    {
-      return json_decode($this->getJson(), 1) ?? [];
-      /**
-       * switch (self::getMethod()) {
-       *    case 'GET':
-       *       return $_GET;
-       *    case 'POST':
-       *       $data = json_decode(file_get_contents('php://input'), 1);
-       *       if (is_null($data))  $data = $_POST;
-       *       if (!empty($_FILES)) $data = array_merge($data, $_FILES);
-       *       return (array) $data;
-       *    default:
-       *       $data = json_decode(file_get_contents('php://input'), 1);
-       *       if (is_null($data)) $data = $_POST;
-       *       return (array) $data;
-       * }
-       */
-   }
-
-   /**
-    * Retorna um objeto com os dados recebidos na requisição
-    * @return object
-    */
-   public function getObj(): object
-   {
-      return json_decode($this->getJson()) ?? (object) [];
-   }
-
-   /**
-    * Retorna um json com os dados recebidos na requisição
-    * @return string
-    */
-   public function getJson(): string
-   {
-      return @$GLOBALS['json'] ?? '';
+      switch ($this->getMethod()) {
+         case 'GET':
+            return $_GET;
+         case 'POST':
+            $data = json_decode(file_get_contents('php://input'), 1);
+            if (is_null($data))  $data = $_POST;
+            if (!empty($_FILES)) $data = array_merge($data, $_FILES);
+            return (array) $data;
+         default:
+            $data = json_decode(file_get_contents('php://input'), 1);
+            if (is_null($data)) $data = $_POST;
+            return (array) $data;
+      }
    }
 
    public static function lowerCaseHeader()
