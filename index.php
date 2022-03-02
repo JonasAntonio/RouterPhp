ba9c0137fe6105aaa03be8ddd34545d5888204b4
JonasAntonio <jayv.jonas@gmail.com>
2022-03-02 13:52:20 -0300

    performance test

--- index.php
+++ index.php
@@ -11,23 +11,40 @@ require_once 'vendor/autoload.php';
 
 $router = new Router();
 
-$router->get('/test', function (Request $request) {
-    echo "b";
+for ($i = 0; $i < 1000; $i++) {
+
+    $router->get("/test$i/{id}", function (Request $request, $id) {
+        echo "{id}: $id";
+    });
+
+    $router->get("/user$i/{id}", function (Request $request, $id) {
+        echo "{id}: $id";
+    });
+
+    $router->get("/test$i/{a}/{b}", function (Request $request, $a, $b) {
+        echo "{a}: $a . {b}: $b";
+    });
+}
+
+$router->get("/no-path-parameter", function (Request $request) {
+    echo "No path parameter";
 });
 
-$router->get('/test/{id}', function (Request $request, $id) {
-    echo $id;
+$router->get("/test/{a}/testing/{b}", function (Request $request, $a, $b) {
+    echo "{a}: $a . {b}: $b";
 });
 
 
-$router->get('/test/{a}/{b}', function (Request $request, $a, $b) {
-    echo "$a . $b";
-});
-
-$router->get('/test/{a}/testing/{b}', function (Request $request, $a, $b) {
-    echo "$a . $b";
-});
-
+$start = microtime(true);
 
 $runner = new Runner($router);
 $runner->run();
+
+echo "<br><br>";
+
+$end = microtime(true);
+
+$time = $end - $start;
+$memory = memory_get_usage(true) / 1048576;
+echo "Memory: $memory <br>";
+echo "Time: $time";
